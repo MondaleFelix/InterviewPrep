a0b13662028bbc5259b36ac07c19ecb6d780f922
Mondale Felix <mondalefelix@gmail.com>
2022-12-04 06:06:00 -0800

    Container With Most Water Python Solution

--- ContainerWithMostWater/container_with_most_water.py
+++ ContainerWithMostWater/container_with_most_water.py
@@ -25,3 +25,30 @@ Output: 1
 
 """
 
+def container_with_most_water(nums):
+    left = 0
+    right = len(nums) - 1
+
+    max_area = 0
+
+    while left < right:
+
+        l_container = nums[left]
+        r_container = nums[right]
+
+        area = min(l_container, r_container) * (right - left)
+
+        max_area = max(area, max_area)
+
+
+        if l_container > r_container:
+            right -= 1
+        elif r_container < l_container:
+            left += 1
+        else:
+            left += 1
+
+    return max_area
+
+print(container_with_most_water([1,8,6,2,5,4,8,3,7]))
+print(container_with_most_water([1,1]))
