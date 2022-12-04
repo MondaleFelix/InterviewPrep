351c8e82f404c954ab03bf65c8df1ade89aac374
Mondale Felix <mondalefelix@gmail.com>
2022-12-04 06:11:08 -0800

    Container With Most Water Swift Solution 🕊 ✅

---
+++ ContainerWithMostWater/ContainerWithMostWater.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+// Container With Most Water
+
+/*
+ 
+ Problem Statement
+
+ You are given an integer array height of length n.
+ There are n vertical lines drawn such that the two endpoints
+ of the ith line are (i, 0) and (i, height[i]).
+
+ Find two lines that together with the x-axis form a container,
+ such that the container contains the most water.
+
+ Return the maximum amount of water a container can store.
+
+ Notice that you may not slant the container.
+
+ Input: height = [1,8,6,2,5,4,8,3,7]
+ Output: 49       0 1 2 3 4 5 6 7 8
+ Explanation: The above vertical lines are represented by
+ array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water
+ (blue section) the container can contain is 49.
+
+ Input: height = [1,1]
+ Output: 1
+
+ */
+
+func containerWithMostWater(nums:[Int]) -> Int {
+    
+    var left = 0
+    var right = nums.count - 1
+    var maxArea = 0
+    
+    while left < right {
+        var leftContainer = nums[left]
+        var rightContainer = nums[right]
+        
+        var area = min(leftContainer, rightContainer) * (right - left)
+        maxArea = max(maxArea, area)
+        
+        if leftContainer > rightContainer {
+            right -= 1
+        } else if rightContainer > leftContainer {
+            left += 1
+        } else {
+            left += 1
+        }
+        
+    }
+    
+    return maxArea
+    
+}
+
+
+containerWithMostWater(nums: [1,8,6,2,5,4,8,3,7])
+containerWithMostWater(nums: [1,1])
