fc789ac1467620e99518fbdaeb0d97851571c16f
MondaleFelix <mondalefelix@yahoo.com>
2024-08-06 15:35:15 -0700

    Added Pangram C#Solution

--- ContainsDuplicate/ContainsDuplicate/ContainsDuplicate/Program.cs
+++ ContainsDuplicate/ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -5,14 +5,15 @@ class Program
     static void Main(string[] args)
     {
         bool test1 = ContainsDuplicate(new int[] { 1, 1, 3, 4, 5, 2 });
+        bool test2 = ContainsDuplicate(new int[] { 1, 9, 3, 4, 5, 2 });
         Console.WriteLine(test1);
+        Console.WriteLine(test2);
         Console.ReadKey();
     }
 
     static bool ContainsDuplicate(int[] nums)
     {
         Dictionary<int, bool> seen = new Dictionary<int, bool>();
-        
 
         foreach (int number in nums)
         {
@@ -24,7 +25,6 @@ class Program
                 seen[number] = true;
             }
         }
-
         return false;
         
     }
---
+++ Pangram/Pangram/Pangram/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Pangram;
+
+/*
+
+Pangram
+
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing English letters (lower or upper-case), return true if sentence is a pangram, or false otherwise.
+
+Note: The given sentence might contain other characters like digits or spaces, your solution should handle these too.
+
+Example 1:
+
+Input: sentence = "TheQuickBrownFoxJumpsOverTheLazyDog"
+Output: true
+Explanation: The sentence contains at least one occurrence of every letter of the English alphabet either in lower or upper case.
+
+Example 2:
+
+Input: sentence = "This is not a pangram"
+Output: false
+Explanation: The sentence doesn't contain at least one occurrence of every letter of the English alphabet.
+
+ */
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool test1 = isPangram("TheQuickBrownFoxJumpsOverTheLazyDog");
+        bool test2 = isPangram("This is not a pangram");
+
+        Console.WriteLine(test1);
+        Console.WriteLine(test2);
+        Console.ReadKey();
+    }
+
+    static bool isPangram(string sentence)
+    {
+        sentence = cleanString(sentence);
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        Dictionary<char, bool> seen = new Dictionary<char, bool>();
+      
+
+        foreach (char letter in sentence)
+        {
+            if (alphabet.Contains(letter) && !seen.ContainsKey(letter))
+            {
+                seen[letter] = true;
+            }
+        }
+
+        if (alphabet.Length == seen.Count) {
+            return true;
+        } else
+        {
+            return false;
+        }
+
+    }
+
+
+
+    static string cleanString(string str)
+    {
+        string cleanedString = str.Replace(" ", "").ToLower();
+        return cleanedString;
+    }
+
+}
+
